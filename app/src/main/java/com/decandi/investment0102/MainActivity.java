c5835203a8f9078e1f3f8a946605595ef63e8f53
JuanP <you@example.com>
2021-01-05 17:22:19 -0400

    Initial commit: Se agregó libreria de terceros Appintro a través de Github Se agrego el enlace del repositorio al archivo build.gradle() Se agrego el enlace de dependencias al archivo build.gradle(module)

---
+++ app/src/main/java/com/decandi/investment0102/MainActivity.java
@@ -0,0 +1,14 @@
+package com.decandi.investment0102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
